2befdbf49c9a3d148076a97df4e0758fa22720d6
Wilmer Bordas <108447599+WBordas56@users.noreply.github.com>
2022-07-26 23:19:58 -0600

    Wilmer

--- app/src/main/java/com/grupo5/proyecto/MainActivity.java
+++ app/src/main/java/com/grupo5/proyecto/MainActivity.java
@@ -12,25 +12,24 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView txtLogo;
     ImageView imageLogo;
-    Animation animation1, animation2;
+    Animation animation1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        txtLogo.setAnimation(animation2);
+
         imageLogo.setAnimation(animation1);
         new Handler().postDelayed(this::run, 4000);
     }
 
     private void init(){
-        txtLogo = findViewById(R.id.textViewGrupo);
+
         imageLogo = findViewById(R.id.imageViewLogo);
         animation1 = AnimationUtils.loadAnimation(this, R.anim.scroll_up);
-        animation2 = AnimationUtils.loadAnimation(this, R.anim.scroll_down);
+
     }
 
     private void run() {
