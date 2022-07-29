bd58ddc94d427d206ee234c82ae82e3b0bf364df
Wilmer Bordas <108447599+WBordas56@users.noreply.github.com>
2022-07-28 23:17:17 -0600

    Update

--- app/src/main/java/com/grupo5/proyecto/ActivityDashboardAdmin.java
+++ app/src/main/java/com/grupo5/proyecto/ActivityDashboardAdmin.java
@@ -6,19 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
-public class ActivityDashboardCliente extends AppCompatActivity {
+public class ActivityDashboardAdmin extends AppCompatActivity {
 
     ImageView salir;
-    LinearLayout tienda, perfil, pedidos, ubicacion, ajustes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard_cliente);
+        setContentView(R.layout.activity_dashboard_admin);
         Init();
         salir.setOnClickListener(this::onClickSalir);
     }
+
+
 
     private void onClickSalir(View view) {
         Intent logout = new Intent(getApplicationContext(), ActivityLogin.class);
@@ -29,6 +30,5 @@ public class ActivityDashboardCliente extends AppCompatActivity {
     private void Init() {
         salir = findViewById(R.id.imgvSalir);
     }
-
 
 }
---
+++ app/src/main/java/com/grupo5/proyecto/ActivityPerfilAdmin.java
@@ -0,0 +1,14 @@
+package com.grupo5.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityPerfilAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_admin);
+    }
+}
