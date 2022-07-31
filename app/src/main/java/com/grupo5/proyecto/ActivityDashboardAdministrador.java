36e071da29deb8d63c7937c05fa8cda78e6b40e2
Wilmer Bordas <108447599+WBordas56@users.noreply.github.com>
2022-07-30 23:18:01 -0600

    Update

---
+++ app/src/main/java/com/grupo5/proyecto/ActivityDashboardAdministrador.java
@@ -0,0 +1,13 @@
+package com.grupo5.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityDashboardAdministrador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_administrador);
+    }
+}
