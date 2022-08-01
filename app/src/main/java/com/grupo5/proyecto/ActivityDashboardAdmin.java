b847e0dd8955afa055044766534a8acb296a4bdb
Wilmer Bordas <108447599+WBordas56@users.noreply.github.com>
2022-07-31 23:38:51 -0600

    Update
    Co-Authored-By: IsaacRP99 <109447099+IsaacRP99@users.noreply.github.com>

--- app/src/main/java/com/grupo5/proyecto/ActivityDashboardAdmin.java
+++ app/src/main/java/com/grupo5/proyecto/ActivityDashboardAdmin.java
@@ -9,17 +9,26 @@ import android.widget.ImageView;
 
 public class ActivityDashboardAdmin extends AppCompatActivity {
 
-    ImageView salir;
-
+    ImageView salir, tienda, perfil, pedidos, info;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_admin);
         Init();
         salir.setOnClickListener(this::onClickSalir);
+        perfil.setOnClickListener(this::onClickPerfil);
+        info.setOnClickListener(this::OnClickInfo);
     }
 
+    private void onClickPerfil(View view) {
+        Intent Perfil = new Intent(getApplicationContext(), ActivityPerfilAdmin.class);
+        startActivity(Perfil);
+    }
 
+    private void OnClickInfo(View view) {
+        Intent info = new Intent(getApplicationContext(), ActivityInfo.class);
+        startActivity(info);
+    }
 
     private void onClickSalir(View view) {
         Intent logout = new Intent(getApplicationContext(), ActivityLogin.class);
@@ -27,7 +36,12 @@ public class ActivityDashboardAdmin extends AppCompatActivity {
         finish();
     }
 
-    private void Init() {
+    private void Init()
+    {
+        tienda = findViewById(R.id.btnTienda);
+        perfil = findViewById(R.id.btnPerfil);
+        pedidos = findViewById(R.id.btnPedidos);
+        info = findViewById(R.id.btnInfo);
         salir = findViewById(R.id.imgvSalir);
     }
 
--- app/src/main/java/com/grupo5/proyecto/ActivityDashboardAdministrador.java
+++ app/src/main/java/com/grupo5/proyecto/ActivityDashboardAdministrador.java
@@ -1,7 +1,15 @@
 package com.grupo5.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class ActivityDashboardAdministrador extends AppCompatActivity {
 
@@ -10,4 +18,5 @@ public class ActivityDashboardAdministrador extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_administrador);
     }
+
 }
