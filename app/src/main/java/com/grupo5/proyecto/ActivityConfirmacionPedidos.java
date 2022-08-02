a3a65e785a36e6bb22145d277af45cafa806fbd4
ZandyCortez <107449527+ZandyCortez@users.noreply.github.com>
2022-08-01 22:52:48 -0600

    Update
    Avance de diseños Zandy

---
+++ app/src/main/java/com/grupo5/proyecto/ActivityConfirmacionPedidos.java
@@ -0,0 +1,14 @@
+package com.grupo5.proyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityConfirmacionPedidos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmacion_pedidos);
+    }
+}
